96a9f984389b12d25256f6f5503d6eb51b62875c
Safisyx <safisyx@gmail.com>
2018-03-06 14:40:31 +0100

    Create Breed Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
+import Breed from './components/Breed'
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1> This is just to test</h1>
+        <Breed/>
       </div>
     );
   }
