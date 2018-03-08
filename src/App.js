084748c02db4d58c2e4f13c9d5195d25bcf46a53
Safisyx <safisyx@gmail.com>
2018-03-08 17:57:32 +0100

    Remove bugs

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import React, { Component } from 'react';
 //import TopTen from './components/TopTen'
 //import Breed from './components/Breed'
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
-<<<<<<< HEAD
-import HomePage from './pages/HomePage'
-=======
 
->>>>>>> master
+import HomePage from './pages/HomePage'
 import Board from './containers/Board'
 import './App.css';
 
 class App extends Component {
-<<<<<<< HEAD
  render() {
    return (
      <Router>
@@ -25,19 +21,6 @@ class App extends Component {
      </Router>
    );
  }
-=======
-  render() {
-    return (
-      <Router>
-        <div>
-          <Route exact path="/dinder" component={Board} />
-          {/*<Route exact path="/products/:id" component={ProductDetails} />*/}
-          <Route exact path="/" render={ () => <Redirect to="/dinder" /> } />
-        </div>
-      </Router>
-    );
-  }
->>>>>>> master
 }
 
 export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,5 @@
 import topTen from './topTen'
 import breed from './breed'
-<<<<<<< HEAD
 import login from './login'
 import currentUser from './currentUser'
 export default {
@@ -8,11 +7,4 @@ export default {
   breed,
   login,
 	currentUser
-=======
-import { popupReducer } from 'react-redux-popup';
-export default {
-  topTen,
-  breed,
-  popup:popupReducer
->>>>>>> master
 }
